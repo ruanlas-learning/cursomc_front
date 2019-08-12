78d1adda0d6014cbf0f163e54d6946425391ca1c
Ruan Silva <ruanluiz.as@gmail.com>
2019-08-11 22:11:33 -0300

    Interceptor para incluir token nas requisicoes

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
@@ -14,13 +14,16 @@ export class ClienteService {
     }
 
     findByEmail(email: string) : Observable<ClienteDTO> {
-        let token = this.storage.getLocalUser().token;
-        let authHeader = new HttpHeaders({'Authorization': 'Bearer ' + token});
+        // let token = this.storage.getLocalUser().token;
+        // let authHeader = new HttpHeaders({'Authorization': 'Bearer ' + token});
+
+        // return this.http.get<ClienteDTO>(
+        //     `${API_CONFIG.baseUrl}/clientes/email?value=${email}`,
+        //     {'headers': authHeader}
+        //     );
 
         return this.http.get<ClienteDTO>(
-            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`,
-            {'headers': authHeader}
-            );
+            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
     getImageFromBucket(id: string) : Observable<any> {
