5a515364a42b9d4d99fc19f8b608bf36237a93b4
Ruan Silva <ruanluiz.as@gmail.com>
2019-08-17 20:21:48 -0300

    Criando pagina de carrinho de compras

---
+++ src/services/domain/cart.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@angular/core";
+import { StorageService } from "../storage.service";
+import { Cart } from "../../models/cart";
+import { ProdutoDTO } from "../../models/produto.dto";
+
+@Injectable()
+export class CartService {
+
+    constructor(public storage : StorageService) {
+    }
+
+    createOrClearCart() : Cart {
+        let cart: Cart = {itens: []};
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    getCart() : Cart {
+        let cart: Cart = this.storage.getCart();
+        if (cart == null) {
+            cart = this.createOrClearCart();
+        }
+        return cart;
+    }
+
+    addProduto(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.itens.findIndex(
+            item => item.produto.id == produto.id
+        );
+        if (position == -1) {
+            cart.itens.push(
+                {
+                    quantidade:1, 
+                    produto: produto
+                }
+            );
+        }else{
+            cart.itens[position].quantidade += 1;
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+}
