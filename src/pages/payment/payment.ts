9376106ea12824780a86522c554ef8423c575939
Ruan Silva <ruanluiz.as@gmail.com>
2019-08-20 20:47:10 -0300

    Tela de confirmacao de pedido

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -43,7 +43,8 @@ export class PaymentPage {
 
   nextPage() {
     this.pedido.pagamento = this.formGroup.value;
-    console.log(this.pedido);
+    // console.log(this.pedido);
+    this.navCtrl.setRoot('OrderConfirmationPage', { pedido: this.pedido });
   }
 
 }
