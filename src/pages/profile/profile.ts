eba89774eabcfe912a345dae8e275ed987eb7597
Ruan Silva <ruanluiz.as@gmail.com>
2019-08-21 22:09:17 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -34,6 +34,10 @@ export class ProfilePage {
 
   ionViewDidLoad() {
     // console.log('ionViewDidLoad ProfilePage');
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
@@ -88,4 +92,20 @@ export class ProfilePage {
       });
   }
 
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(
+        response => {
+          this.picture = null;
+          this.loadData();
+        },
+        error => {
+
+        }
+      );
+  }
+
+  cancel() {
+    this.picture = null;
+  }
 }
