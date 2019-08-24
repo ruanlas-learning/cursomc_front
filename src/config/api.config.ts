a5e0b272eae0ea6c04eac2a352c00b10af04e4b9
Ruan Silva <ruanluiz.as@gmail.com>
2019-08-24 18:39:12 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,6 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
-    bucketBaseUrl: "http://localhost:8080/arquivos/show"
+    // baseUrl: "http://localhost:8080",
+    baseUrl: "http://cursospringionic.ddns.net:8080",
+    // bucketBaseUrl: "http://localhost:8080/arquivos/show"
+    bucketBaseUrl: "http://cursospringionic.ddns.net:8080/arquivos/show"
 }
